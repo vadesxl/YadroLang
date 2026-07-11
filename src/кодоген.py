1075d6b74864d88668c9254c48793e810845449d
vadesxl <39384856+vadesxl@users.noreply.github.com>
2026-07-11 02:07:24 +0200

    Явно внедрить ABI symbol mangler

--- src/кодоген.py
+++ src/кодоген.py
@@ -3,9 +3,9 @@ from src import кодоген_verified as _backend
 from src.abi import symbol
 from src.типы_sound import ЗвуковаяПроверкаТипов
 from src.этика import ИСТОЧНИКИ,СТОКИ,САНИТАЙЗЕРЫ
-_backend.символ=symbol
 ОшибкаКодогена=_backend.ОшибкаКодогена
 class Кодоген(_backend.Кодоген):
+ def __init__(self):super().__init__(symbol_mangler=symbol)
  def сгенерировать(self,программа):
   ЗвуковаяПроверкаТипов(set(ИСТОЧНИКИ)|set(СТОКИ)|set(САНИТАЙЗЕРЫ)|{"печать"}).проверить(программа)
   return super().сгенерировать(программа)
